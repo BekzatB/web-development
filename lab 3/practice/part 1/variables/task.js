a54686c20345852c93718f9f5b274f8e23f47975
BBeshimov <bbeshimov@dar.io>
2022-02-18 08:40:37 +0500

    lab 3

---
+++ lab 3/practice/part 1/variables/task.js
@@ -0,0 +1,19 @@
+let admin, name; // can declare two variables at once
+
+name = "Bekzat";
+
+admin = name;
+
+alert(admin); // "Bekzat"
+
+let ourPlanetName = "Earth";
+
+let currentUserName = "Bekzat";
+
+const birthday = '29.10.1998';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '29.10.1998';
+
+const AGE = someCode(BIRTHDAY);
